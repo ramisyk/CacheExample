9d8a120d2165617f827eb6b5f326d20a5b5ab99e
ramisyk <ramisyk97@gmail.com>
2023-11-23 08:06:40 +0300

    Pub/Sub example

---
+++ Redis.Pubsliher.MessageBroker/Program.cs
@@ -0,0 +1,14 @@
+﻿using StackExchange.Redis;
+
+// ssl host, redis password vb are included to function as parameter like option => {} if they need...
+ConnectionMultiplexer redisConnection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = redisConnection.GetSubscriber();
+
+while (true)
+{
+    Console.Write("Message: ");
+    string message = Console.ReadLine();
+
+    subscriber.PublishAsync("myChannel", message);
+}
---
+++ Redis.Subscriber.MessageBroker/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redisConnection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = redisConnection.GetSubscriber();
+
+await subscriber.SubscribeAsync("myChannel", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+
+Console.Read();
